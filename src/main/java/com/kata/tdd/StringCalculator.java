861349758a4a67e4bf598256a3b9157bce6447fc
saranDevarajan <saranya.devarajan@lease-works.com>
2021-05-24 20:37:03 +0530

    support for custom delimiter  "//[delimiter]\n[numbers...]"

--- src/main/java/com/kata/tdd/StringCalculator.java
+++ src/main/java/com/kata/tdd/StringCalculator.java
@@ -4,15 +4,32 @@ import org.springframework.util.StringUtils;
 
 public class StringCalculator {
 
+    // "//[delimiter]\n[numbers...]"
     public int add(String numbers) {
         if (!StringUtils.hasText(numbers))
             return 0;
 
+        String[] split = numbers.split("\n");
+        Character delimiter = split[0].charAt(split[0].length()-1);
+        String[] nums = split[1].split(delimiter.toString());
+
+        return sum(nums);
+    }
+
+    public int addBasic(String numbers) {
+        if (!StringUtils.hasText(numbers))
+            return 0;
+
         String[] nums = numbers.split(",|\n");
+        return sum(nums);
+    }
+
+    int sum(String[] nums){
         int sum = 0;
         for (String n : nums) {
             sum += Integer.valueOf(n);
         }
         return sum;
     }
+
 }
--- src/test/java/com/kata/tdd/StringCalculatorTest.java
+++ src/test/java/com/kata/tdd/StringCalculatorTest.java
@@ -12,35 +12,42 @@ class StringCalculatorTest {
     @Test
     void addEmptyString() {
         String empty = "";
-        int result = calculator.add(empty);
+        int result = calculator.addBasic(empty);
         assertThat(result).isZero();
     }
 
     @Test
     void addOneNumber() {
         String oneInput = "1";
-        int result = calculator.add(oneInput);
+        int result = calculator.addBasic(oneInput);
         assertThat(result).isOne();
     }
 
     @Test
     void addCommaSeparatedNumbers() {
         String commaSeparatedNumbers = "1,2";
-        int result = calculator.add(commaSeparatedNumbers);
+        int result = calculator.addBasic(commaSeparatedNumbers);
         assertThat(result).isEqualTo(3);
     }
 
     @Test
     void addCommaSeparated_N_Numbers() {
         String commaSeparatedNumbers = "1,2,3";
-        int result = calculator.add(commaSeparatedNumbers);
+        int result = calculator.addBasic(commaSeparatedNumbers);
         assertThat(result).isEqualTo(6);
     }
 
     @Test
     void addCommaSeparatedOrLine_N_Numbers() {
         String commaSeparatedNumbers = "1\n2,3";
-        int result = calculator.add(commaSeparatedNumbers);
+        int result = calculator.addBasic(commaSeparatedNumbers);
         assertThat(result).isEqualTo(6);
+    }
+
+    @Test
+    void addCustomDelimiter() {
+        String input = "//;\n1;2";
+        int result = calculator.add(input);
+        assertThat(result).isEqualTo(3);
     }
 }
