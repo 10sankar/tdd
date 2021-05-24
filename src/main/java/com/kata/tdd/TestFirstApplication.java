a3c85115c91f79a7ca89dce0c7c21cfb2d170366
saranDevarajan <saranya.devarajan@lease-works.com>
2021-05-24 19:28:59 +0530

    first commit

---
+++ src/main/java/com/kata/tdd/TestFirstApplication.java
@@ -0,0 +1,13 @@
+package com.kata.tdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestFirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestFirstApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kata/tdd/TestFirstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kata.tdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestFirstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
